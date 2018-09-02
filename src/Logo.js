951e4f6e319c64f69ce51aa0620e2547dfc40c55
mandeep2809 <mandeepkandhari@gmail.com>
2018-09-02 01:31:22 -0400

    Adjusting the position of the logo image and three bar image and addition of links

--- src/Logo.js
+++ src/Logo.js
@@ -6,13 +6,13 @@ const Logo=()=>
 	{
 	return(
 		<div className='ma0 pa0 shadow-5 bw3 flex justify-between'>
-			<div className='ma1 grow'>
-				<img className='br-100 image pv2  ml4-l ml3-ns pointer' src={Logos} 
-				width={100} height={100} alt='logo' />
+			<div className='mh2 mt3 grow'>
+				<img className='br-100  pv2  ml4-l ml3-ns pointer' src={Logos} 
+				width={80} height={80} alt='logo' />
 			</div>
 			
 				<nav className=' mt4-l mr2 dropdown '>
-					<FontAwesome name='bars' className='dib dropbtn  mt3 pa2' />
+					<FontAwesome name='bars' className='dib dropbtn  mt2 pa2' />
 					<ul className='pa2-l pa0 ma2 tc f3 br2 dropdown-content list'>
 						<li><a href='#about'>About</a></li>
 						<li><a href='#work'>Work</a></li>
